d469458e827fe2de6050d0b8a2f376a5fc091329
ALEXANDRA ORTEGA CUBILLOS <alexandra-ortegac@unilibre.edu.co>
2019-08-21 06:07:43 -0500

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,38 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculadora';
+export class AppComponent 
+{
+  numero1:number; numero2:number; Resultado:number;
+  Calculo(numero1:number,num2:number,cal:string): number
+  {
+    switch(cal)
+    {
+      case "sumar":
+        return this.Resultado = this.numero1 + this.numero2;
+      case "restar":
+        return this.Resultado = this.numero1 - this.numero2;
+      case "multiplicacion":
+        return this.Resultado = this.numero1 * this.numero2;
+      case "dividir":
+        if(this.numero2 == 0)
+        {
+        alert("IMPOSIBLE DIVIDIR POR CERO!!!");
+        return null;
+        }
+        else
+        {
+        return this.Resultado = this.numero1 / this.numero2;
+        }
+      case "modulo":
+        return this.Resultado = this.numero1 % this.numero2;
+        case "limpiar":
+            return null;
+    }
+  }
+  rest(){
+    this.Resultado = null;
+    this.numero1 = null;
+    this.numero2 = null;
+  }
 }
